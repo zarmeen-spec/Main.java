c2d0c9a365895d453dafa18357bc4e851df3db3b
zarmeen <zarmeenkhaliq5@gmail.com>
2024-12-01 15:34:23 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-class Main {
+class Test {
     public static void main(String[] args){
-        System.out.println("helloworld")
+        System.out.println("helloworld");
+        System.out.println("helloworld");
     }
 }
